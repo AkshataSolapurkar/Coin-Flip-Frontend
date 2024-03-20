11d28a8c12705fca81fdea8a64be5d9c02b49574
AkshataSolapurkar <solapurkarakshata@gmail.com>
2024-03-20 22:46:38 +0530

    updating the takebate.tsx

--- src/app/takebate/page.tsx
+++ src/app/takebate/page.tsx
@@ -1,28 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
+import Navbar from '../components/Navbar'
 
 export default function takebate() {
   return (
-    <div>
+  
       <div className="container m-auto p-[0px] box-border">
-        <header  className="px-[10px] flex mx-14 my-[30px] justify-between items-center">
-            
-                <div className="flex justify-center gap-3">
-                    <Image width={30} height={30} src="/Logomark.png" alt="Logo"/>
-                    <span className="text-white font-bold text-[20px]">CoinFlip</span>
-                    </div>
-                    <div className="flex justify-between gap-6 ">
-                       <button className="text-white font-medium text-[16px]" >Learn More</button>
-                       <button className="bg-[#7F56D9] p-[10px] rounded-md text-white font-[700] text-[16px]">Connect Wallet</button>
-                    </div>
-        
-            
-            
-        </header>
+        <Navbar/>
         <div className="herohead">
             <div className="">
         
-            <Image className="left-custom relative" fill src="/cryptocurrency-casino 1.png" alt="pic"/>
+            <Image className="relative -left-[35%]" width={1290} height={1290} src="/cryptocurrency-casino 1.png" alt="pic" />
                 <div className="top-[35%] absolute flex flex-col justify-center items-center left-1/2 max-w-[70%] -translate-x-1/2 bg-white shadow-md rounded-lg">
                     <div  className="px-[100px] pt-[50px] pading  text-center">
                     <h2 className="text-[#212121] text-center font-bold md:text-[36px] text-[20px]">Make your bet, toss your coin</h2>
@@ -46,13 +34,11 @@ export default function takebate() {
                         <button className="text-center btn bg-[#2A1853] text-white flex justify-center">Place Your Bet</button>
                       </div>
                     </div>
-                    
+                    </div>
+                    </div>
             </div>
         </div>
     </div>
-</div>
-</div>
-</div>
   );
 }
 
